47a59ed6fbd42b7b8b97a445fdda11eab56b956f
Anshuman <anshuman.a@myntra.com>
2021-12-02 23:29:03 +0530

    Testing get performance

--- src/main/java/com/test/jmh/redis/ExecutionPlan.java
+++ src/main/java/com/test/jmh/redis/ExecutionPlan.java
@@ -2,20 +2,32 @@ package com.test.jmh.redis;
 
 import com.test.jmh.redis.clients.Jedis;
 import com.test.jmh.redis.clients.Lettuce;
+import com.test.jmh.utils.RandomUtils;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @State(Scope.Benchmark)
 public class ExecutionPlan {
 
-//    public Jedis jedis;
+    public Jedis jedis;
     public Lettuce lettuce;
+    public List<String> randomStringList;
 
     @Setup(Level.Trial)
     public void setUp() {
-//        jedis = new Jedis();
+        jedis = new Jedis();
         lettuce = new Lettuce();
+        randomStringList = new ArrayList<>();
+
+        for (int i = 0; i < 100000; i++) {
+            String random = RandomUtils.getRandomString();
+            randomStringList.add(random);
+            jedis.set(random, RandomUtils.getRandomString());
+        }
     }
 }
--- src/main/java/com/test/jmh/redis/RedisTest.java
+++ src/main/java/com/test/jmh/redis/RedisTest.java
@@ -1,6 +1,5 @@
 package com.test.jmh.redis;
 
-import com.test.jmh.utils.RandomUtils;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
@@ -12,11 +11,25 @@ public class RedisTest {
 //    public void randomKeySetJedisTest(ExecutionPlan executionPlan) {
 //        executionPlan.jedis.set(RandomUtils.getRandomString(), RandomUtils.getRandomString());
 //    }
+//
+//    @Benchmark
+//    @BenchmarkMode(Mode.Throughput)
+//    public void randomKeySetLettuceTest(ExecutionPlan executionPlan) {
+//        executionPlan.lettuce.set(RandomUtils.getRandomString(), RandomUtils.getRandomString());
+//    }
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
-    public void randomKeySetLettuceTest(ExecutionPlan executionPlan) {
-        executionPlan.lettuce.set(RandomUtils.getRandomString(), RandomUtils.getRandomString());
+    public void randomKeyGetJedisTest(ExecutionPlan executionPlan) {
+        for (String random: executionPlan.randomStringList)
+            executionPlan.jedis.get(random);
+    }
+
+    @Benchmark
+    @BenchmarkMode(Mode.Throughput)
+    public void randomKeyGetLettuceTest(ExecutionPlan executionPlan) {
+        for (String random: executionPlan.randomStringList)
+            executionPlan.lettuce.get(random);
     }
 }
 
