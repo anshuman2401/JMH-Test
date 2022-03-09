2581a68d83e71f12c046bfcd1a57e0aae116db13
Anshuman <anshuman.a@myntra.com>
2022-03-09 11:54:00 +0530

    Removing uidx and pidx from the value

--- src/main/java/com/test/jmh/redis/utils/Transformer.java
+++ src/main/java/com/test/jmh/redis/utils/Transformer.java
@@ -13,21 +13,19 @@ public class Transformer {
 
     public static ProfileEntry createRandomProfile() {
         ProfileEntry entry = new ProfileEntry();
-        entry.setUidx(UUID.randomUUID().toString());
-        entry.setPidx(UUID.randomUUID().toString());
-        entry.setName("Anshuman");
+//        entry.setUidx(UUID.randomUUID().toString());
+//        entry.setPidx(UUID.randomUUID().toString());
+        entry.setName("Anshuman Kaushik");
         entry.setDefaultProfile(true);
-        entry.setImage("some url which is long one because it is a image url");
+        entry.setImage("http://assets.myntassets.com/assets/images/2021/5/12/49d1f015-5e87-4650-b30a-4f638461cfe21620808191515-temp_photo.jpg");
         return entry;
     }
 
     public static Map<String, String> createRandomProfileMap() {
         Map<String, String> entry = new HashMap<>();
-        entry.put("uidx", UUID.randomUUID().toString());
-        entry.put("pidx", UUID.randomUUID().toString());
         entry.put("name", "Anshuman");
         entry.put("defaultProfile", "true");
-        entry.put("image", "some url which is long one because it is a image url");
+        entry.put("image", "http://assets.myntassets.com/assets/images/2021/5/12/49d1f015-5e87-4650-b30a-4f638461cfe21620808191515-temp_photo.jpg");
         return entry;
     }
 
